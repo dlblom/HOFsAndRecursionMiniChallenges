8d69b69a69f1292f949f82ab9870530b902aecf2
Danielle Blom <dlblom91@gmail.com>
2020-12-22 15:06:01 -0500

    Update higherOrderFunctions to remove variables and add note for students to reference test file to see how variables are structured

--- src/higherOrderFunctions.js
+++ src/higherOrderFunctions.js
@@ -1,128 +1,36 @@
+// READ THIS BEFORE GETTING STARTED: Check out the tests in higherOrderFunctions.spec.js to see how the test variable data is structured. This will help you figure out how to write your functions.
+
 // TODO: Using the .map() method, complete the getCitiesPopulations so that it returns an array of the city populations only
-function getCitiesPopulations({
-    city: `Austin`,
-    pop: 964254,
-  },
-  {
-    city: `Waco`,
-    pop: 138183,
-  },
-  {
-    city: `Leander`,
-    pop: 56111,
-  },
-  {
-    city: `Cedar Park`,
-    pop: 76999,
-  }) {
-    //function logic goes here
-  };
+function getCitiesPopulations(arrayOfObjects) {
+  //function logic goes here
+}
 
 //TODO: Using the .map() method, complete the addToPopulations function so that it returns an array of populations with 100 added to each population
-function addToPopulations([
-  {
-    city: `Austin`,
-    pop: 964254,
-  },
-  {
-    city: `Waco`,
-    pop: 138183,
-  },
-  {
-    city: `Leander`,
-    pop: 56111,
-  },
-  {
-    city: `Cedar Park`,
-    pop: 76999,
-  }
-  ]) {
-    // funciton logic goes here
-  };
-
+function addToPopulations(arrayOfObjects) {
+  // TODO: funciton logic goes here
+}
 
 // TODO: Using the .filter() method, complete the getDogsUnderFive function so that it returns an array of only the dog objects with ages under 5
-function getDogsUnderFive([
-  {
-    name: `Tucker`,
-    age: 1,
-    breed: `Lab`,
-  },
-  {
-    name: `Spot`,
-    age: 4,
-    breed: `Mutt`,
-  },
-  {
-    name: `Waffles`,
-    age: 6,
-    breed: `Corgi`,
-  },
-  {
-    name: `Sheldon`,
-    age: 11,
-    breed: `Corgi`,
-  },
-]) {
-  // function logic goes here
-};
+function getDogsUnderFive(arrayOfObjects) {
+  // TODO: function logic goes here
+}
 
 // TODO: Using the .filter() method, complete the overFirstIndex function so that it returns an array of objects at indexes greater than one
-function overFirstIndex([
-  {
-    name: `Tucker`,
-    age: 1,
-    breed: `Lab`,
-  },
-  {
-    name: `Spot`,
-    age: 4,
-    breed: `Mutt`,
-  },
-  {
-    name: `Waffles`,
-    age: 6,
-    breed: `Corgi`,
-  },
-  {
-    name: `Sheldon`,
-    age: 11,
-    breed: `Corgi`,
-  },
-]) {
-  // function logic goes here
-};
+function overFirstIndex(arrayOfObjects) {
+  // TODO: function logic goes here
+}
 
 // TODO: Using the .reduce() method, complete the getSumOfNumbers function so that it returns the sum of an array of numbers
-function getSumOfNumbers([1, 2, 3, 4]) {
-  // function logic goes here
-};
+function getSumOfNumbers(arrayOfNumbers) {
+  // TODO: function logic goes here
+}
 
 // TODO: Using the .reduce() method, complete the  getPopulationSum funciton so that is returns the sum of every city's population except Waco
-function getPopulationSum([
-  {
-    city: `Austin`,
-    pop: 964254,
-  },
-  {
-    city: `Waco`,
-    pop: 138183,
-  },
-  {
-    city: `Leander`,
-    pop: 56111,
-  },
-  {
-    city: `Cedar Park`,
-    pop: 76999,
-  },
-]) {};
+function getPopulationSum(arrayOfObjects) {
+  //TODO: funciton logic goes here
+}
 
-// TODO: Using the .reduce() method, complete the flattenArray funciton so that it flattens the nested array (result should be [0, 1, 2, 3, 4, 5])
-function flattenArray([
-  [0, 1],
-  [2, 3],
-  [4, 5],
-]) {
-  // funcion logic goes here
-};
+// TODO: Using the .reduce() method, complete the flattenArray funciton so that it flattens the nested array
+function flattenArray(arrayOfArrays) {
+  // TODO: funcion logic goes here
+}
